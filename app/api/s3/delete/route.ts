d9ecd4c033ab00a3b2f09469a8b2e9df08c91a83
Abdullah Awad <djdgdnnxcbj@gmail.com>
2026-07-17 19:49:09 +0300

    feat: implement session validation and request protection for S3 delete route

--- app/api/s3/delete/route.ts
+++ app/api/s3/delete/route.ts
@@ -1,9 +1,42 @@
+import arcjet, { detectBot, fixedWindow } from '@/lib/arcjet';
+import { auth } from '@/lib/auth';
 import { S3 } from '@/lib/S3Clinet';
 import { DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: 'LIVE',
+      allow: [],
+    }),
+  )
+  .withRule(
+    fixedWindow({
+      mode: 'LIVE',
+      window: '1m',
+      max: 5,
+    }),
+  );
+
 export async function DELETE(request: Request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   try {
+    const decision = await aj.protect(request, {
+      fingerprint: session?.user.id as string,
+    });
+
+    if (decision.isDenied()) {
+      return NextResponse.json(
+        {
+          error: 'duuud thats not gooood',
+        },
+        { status: 429 },
+      );
+    }
     const body = await request.json();
     const key = body.key;
 
