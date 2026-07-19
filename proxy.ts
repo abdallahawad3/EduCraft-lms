ea605f43735b0207dd65ace235bb703b11de8768
Abdullah Awad <djdgdnnxcbj@gmail.com>
2026-07-19 06:39:11 +0300

    feat: implement access control in proxy function using arcjet

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,37 @@
 import { auth } from '@/lib/auth';
+import arcjet, { detectBot, fixedWindow } from '@arcjet/next';
 import { headers } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    detectBot({
+      mode: 'LIVE',
+      allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:MONITOR', 'CATEGORY:PREVIEW'],
+    }),
+    fixedWindow({
+      mode: 'LIVE',
+      window: '1m',
+      max: 5,
+    }),
+  ],
+});
+
 export async function proxy(request: NextRequest) {
+  const decision = await aj.protect(request);
+
+  if (decision.isDenied()) {
+    return NextResponse.json(
+      {
+        error: 'Access denied',
+      },
+      {
+        status: 403,
+      },
+    );
+  }
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
